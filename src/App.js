3f60ad3a954aad747be0c58e6c50ba8b8bd6625b
ArielGad <ariel.gad4@gmail.com>
2019-02-23 22:35:24 +0200

    handle list of objects & destructuring assignment

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import React, { Component } from 'react';
 import Ninjas from './Ninjas';
 
 class App extends Component {
+
+    state = {
+        ninjas: [
+            {name: "Ariel", age: 32, belt: "black", id:1},
+            {name: "Foo", age: 23, belt: "brown", id:2},
+            {name: "Bar", age: 27, belt: "blue", id:3},
+        ]
+    }
+
     render(){
         return (
             <div>
                 <h1>Hello, World!</h1>
                 <p>Welcome to my React mini project</p>
-                <Ninjas name="Ariel" age="32" belt="Black"/>
+                <Ninjas ninjas={this.state.ninjas}/>
             </div>
         )
     }

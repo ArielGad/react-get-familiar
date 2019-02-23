1d200357e67de1efb24149b800a5a6a4c66f3fd1
ArielGad <ariel.gad4@gmail.com>
2019-02-23 22:02:16 +0200

    nesting componenets

--- src/App.js
+++ src/App.js
@@ -1,32 +1,13 @@
 import React, { Component } from 'react';
+import Ninjas from './Ninjas';
 
 class App extends Component {
-    state = {
-        firstName: "Ariel",
-        lastName: "Gad",
-    };
-
-    handleChange = (e) =>{
-        this.setState({firstName: e.target.value});
-    };
-
-    handleSubmit = (e) => {
-    //    prevents from the browser to refresh.
-    //    since it is the natural behavior of the submit
-        e.preventDefault();
-        console.log("Form submitted with value " + this.state.firstName)
-    };
-
     render(){
         return (
             <div>
                 <h1>Hello, World!</h1>
-                <p>My name is: {this.state.firstName}</p>
-
-                <form onSubmit={this.handleSubmit}>
-                    <input type="text" onChange={this.handleChange}/>
-                    <button>Submit</button>
-                </form>
+                <p>Welcome to my React mini project</p>
+                <Ninjas />
             </div>
         )
     }
